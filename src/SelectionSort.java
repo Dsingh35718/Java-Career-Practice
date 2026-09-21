f4b26677fed9118275150b9c171fd605662ffd5d
Darshdeep Singh <Dsingh35@oldwestbury.edu>
2026-09-21 15:37:54 -0400

    Add selection sort practice

---
+++ src/SelectionSort.java
@@ -0,0 +1,27 @@
+public class SelectionSort {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {64, 25, 12, 22, 11};
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+
+            int minIndex = i;
+
+            for (int j = i + 1; j < numbers.length; j++) {
+
+                if (numbers[j] < numbers[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = numbers[i];
+            numbers[i] = numbers[minIndex];
+            numbers[minIndex] = temp;
+        }
+
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+    }
+}
